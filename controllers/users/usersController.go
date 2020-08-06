cc6145bfc20a3bcf1f1c25e5210e6d7ad528e23b
hsimao <e087754958@gmail.com>
2020-08-06 21:06:07 +0800

    feat: CreateUser Bad Request Error 封裝

--- controllers/users/usersController.go
+++ controllers/users/usersController.go
@@ -4,10 +4,30 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hsimao/bookstore_users-api/domain/users"
+	"github.com/hsimao/bookstore_users-api/services"
+	"github.com/hsimao/bookstore_users-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+
+	// 參數格式錯誤
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
